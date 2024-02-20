6d78982d6fc150a6adcc103091cd728c92722f0a
Trevor <trevor@quanta-inc.com>
2024-02-20 18:54:54 -0500

    feat: auto set href

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
-	import { slide, fade } from 'svelte/transition';
+	import { slide } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
 
-	const domains = ['xyz', 'zip', 'kiwi'];
-	let activeDomain = domains[0];
+	const domains = [
+		{
+			href: 'https://trev.xyz',
+			tld: 'xyz'
+		},
+		{
+			href: 'https://trev.kiwi',
+			tld: 'kiwi'
+		},
+		{
+			href: 'https://trev.zip',
+			tld: 'zip'
+		},
+		{
+			href: 'https://xn--rev-g0a.com/',
+			tld: 'com'
+		}
+	];
+	let activeDomain = domains.find((domain) => domain.href === $page.url.href) ?? domains[0];
 
 	function changeDomain() {
 		const index = domains.indexOf(activeDomain);
@@ -19,11 +37,11 @@
 			id: 'tsparticles',
 			options: {
 				preset: 'stars',
-                particles: {
-                    move: {
-                        speed: 0.5
-                    }
-                }
+				particles: {
+					move: {
+						speed: 0.5
+					}
+				}
 			}
 		});
 	});
@@ -35,16 +53,18 @@
 	></script>
 </svelte:head>
 
-<div class="flex items-center justify-center h-screen bg-black">
-	<h1 class="text-5xl font-bold text-white z-10">trev.</h1>
-	<div class="w-24 content-center grid cols-start-1 cols-end-2 rows-start-1 rows-end-2">
-		{#key activeDomain}
-			<div
-				transition:slide={{ duration: 1000, easing: quintOut }}
-				class="text-5xl font-bold text-white z-10 h-24 flex items-center"
-			>
-				{activeDomain}
-			</div>
-		{/key}
-	</div>
+<div class="flex flex-col items-center justify-center h-screen bg-black">
+	<a href={activeDomain.href} class="flex items-center h-24">
+		<h1 class="font-sans text-5xl font-bold text-white z-10">trev.</h1>
+		<div class="w-24 content-center grid cols-start-1 cols-end-2 rows-start-1 rows-end-2">
+			{#key activeDomain}
+				<div
+					transition:slide={{ duration: 1000, easing: quintOut }}
+					class="text-5xl font-bold text-white z-10 h-24 flex items-center"
+				>
+					{activeDomain.tld}
+				</div>
+			{/key}
+		</div>
+	</a>
 </div>
